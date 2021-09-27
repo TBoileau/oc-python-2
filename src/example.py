def81d47d3fdc8e74bcbda9a785d7ab3ceb3e8ff
TBoileau <t-boileau@email.com>
2021-09-28 01:00:18 +0200

    build: install pytest and some coding style tools (#5,#6)

---
+++ src/example.py
@@ -0,0 +1,29 @@
+"""Imported modules."""
+
+
+class Example:
+    """Example"""
+
+    def __init__(self, name: str):
+        """
+        Constructor
+
+        :param name:
+        """
+        self.name: str = name
+
+    def test(self):
+        """
+        Test
+
+        :return:
+        """
+        return self.name
+
+    def test2(self):
+        """
+        Test
+
+        :return:
+        """
+        return self.name
